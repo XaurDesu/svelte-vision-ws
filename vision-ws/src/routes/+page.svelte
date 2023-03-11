a1e4724e3ff5f40c54621dcdb871de03c24e617c
Jaime Torres <andrestbermejoj@gmail.com>
2023-03-11 09:36:19 -0500

    add a tiny video prototype

--- vision-ws/src/routes/+page.svelte
+++ vision-ws/src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
+<video
+	poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg"
+	src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4"
+	
+	<track
+	    kind="captions"
+    >
+    
+	></video
+>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
